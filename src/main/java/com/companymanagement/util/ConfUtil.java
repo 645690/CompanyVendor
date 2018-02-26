5c3a8dbce6b610e31ceceb256be3237fcdfe1de0
tk125 <tsukian125@gmail.com>
2018-02-26 14:12:51 +0800

    tk confug util

--- src/main/java/com/companymanagement/util/ConfUtil.java
+++ src/main/java/com/companymanagement/util/ConfUtil.java
@@ -5,13 +5,18 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.springframework.util.ResourceUtils;
+
 public class ConfUtil {
 	
 	static Properties props = new Properties();
 	
 	static {
 		try {
-			FileInputStream fis = new FileInputStream("config.properties");
+			
+			FileInputStream fis = new FileInputStream(ResourceUtils.getFile("classpath:config.properties"));
+			//used when you put config.properties into target folder
+			//FileInputStream fis = new FileInputStream("config.properties");
 			props.load(fis);
 			fis.close();
 		} catch (FileNotFoundException e) {
