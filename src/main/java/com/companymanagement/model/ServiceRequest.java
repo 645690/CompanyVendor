d47067bff96846caa723dee2492f84b759d7edad
645690 <645690@users.noreply.github.com>
2018-02-26 16:35:52 +0800

    SN ServiceRequest

--- src/main/java/com/companymanagement/model/ServiceRequest.java
+++ src/main/java/com/companymanagement/model/ServiceRequest.java
@@ -14,7 +14,8 @@ import javax.persistence.OneToMany;
 @Entity
 @NamedQueries({
 		@NamedQuery(name = "ServiceRequest.findRegNo", query = "SELECT sr FROM ServiceRequest sr WHERE sr.regNo=:regNo"),
-		@NamedQuery(name = "ServiceRequest.findByCompany", query = "SELECT sr FROM ServiceRequest sr WHERE sr.company=:company") })
+		@NamedQuery(name = "ServiceRequest.findByCompany", query = "SELECT sr FROM ServiceRequest sr WHERE sr.company=:company"),
+		@NamedQuery(name = "ServiceRequest.findByDepartmentAndCompany", query = "SELECT sr FROM ServiceRequest sr WHERE sr.company=:company AND sr.department=:department") })
 public class ServiceRequest extends Base {
 
 	private static final long serialVersionUID = -4697330149497262611L;
--- src/main/java/com/companymanagement/service/ServiceRequestService.java
+++ src/main/java/com/companymanagement/service/ServiceRequestService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.companymanagement.common.CompanyMgmtException;
 import com.companymanagement.model.Company;
+import com.companymanagement.model.Employee;
 import com.companymanagement.model.ServiceRequest;
 import com.companymanagement.model.ServiceRequestApplication;
 
@@ -20,5 +21,8 @@ public interface ServiceRequestService extends BaseService {
 	void addServiceRequestApplication(Long srRegNo, ServiceRequestApplication srApp) throws CompanyMgmtException;
 
 	List<ServiceRequest> findServiceRequestsByCompany(Company company) throws CompanyMgmtException;
+
+	List<ServiceRequest> findServiceRequestsByEmployeeDepartmentAndCompany(Employee employee)
+			throws CompanyMgmtException;
 
 }
