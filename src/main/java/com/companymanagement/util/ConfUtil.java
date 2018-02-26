ab6449673ea0b38d1b35aa5f160636956267b0ca
645690 <645690@users.noreply.github.com>
2018-02-26 16:56:41 +0800

    Revert "Merge branch 'master' of https://github.com/645690/CompanyVendor"
    This reverts commit 540902ac1822f00e94c630cf5f8fdf04c9d64a31, reversing
    changes made to ec83af9ff0755c8b064a35cf8f06de8331d32c35.

--- src/main/java/com/companymanagement/util/ConfUtil.java
+++ src/main/java/com/companymanagement/util/ConfUtil.java
@@ -8,15 +8,12 @@ import java.util.Properties;
 import org.springframework.util.ResourceUtils;
 
 public class ConfUtil {
-	
+
 	static Properties props = new Properties();
-	
+
 	static {
 		try {
-			
 			FileInputStream fis = new FileInputStream(ResourceUtils.getFile("classpath:config.properties"));
-			//used when you put config.properties into target folder
-			//FileInputStream fis = new FileInputStream("config.properties");
 			props.load(fis);
 			fis.close();
 		} catch (FileNotFoundException e) {
@@ -27,7 +24,7 @@ public class ConfUtil {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static String get(String key) {
 		return props.getProperty(key);
 	}
